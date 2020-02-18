9fbaf99c919bbf461069a5db4e23bcb33815ff18
Rupam55 <rupamkarmakar55@gmail.com>
2020-02-19 00:05:25 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+var a=0;
+var y = new Boolean(false);
 
+
+function call(i){
+    setTimeout(function() { 
+        if(a==99){
+            y=true;
+        }else if(a==0){
+            y=false;
+        }
+        if(y==true){
+            a--;
+        }else{
+            a++;
+        }
+        console.log(a);
+        document.getElementById("title").style.backgroundImage=" radial-gradient(circle at center, #00a8cc 0%, #27496d "+a+"%, #112d4e 100%)";
+    }, 30*i);
+    
+}
+for(let i=0;i<300;i++){
+    call(i);
+}   
